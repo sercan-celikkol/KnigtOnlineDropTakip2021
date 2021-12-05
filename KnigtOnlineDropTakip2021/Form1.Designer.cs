1ee01a59e9a5fe1f081d9f8ade7c35b3508ca644
PC <PC@DESKTOP-J2L0HD8>
2021-12-05 15:51:26 +0300

    Add project files.

---
+++ KnigtOnlineDropTakip2021/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace KnigtOnlineDropTakip2021
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.btnYeniEkle = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.txtBoxDropName = new System.Windows.Forms.TextBox();
+            this.txtBoxPrice = new System.Windows.Forms.TextBox();
+            this.txtBoxPartyMembers = new System.Windows.Forms.TextBox();
+            this.maskedTxtDropDate = new System.Windows.Forms.MaskedTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(4, 153);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.Size = new System.Drawing.Size(764, 452);
+            this.dataGridView2.TabIndex = 0;
+            this.dataGridView2.ColumnAdded += new System.Windows.Forms.DataGridViewColumnEventHandler(this.dataGridView2_ColumnAdded);
+            this.dataGridView2.UserDeletingRow += new System.Windows.Forms.DataGridViewRowCancelEventHandler(this.dataGridView2_UserDeletingRow);
+            // 
+            // btnYeniEkle
+            // 
+            this.btnYeniEkle.Location = new System.Drawing.Point(388, 12);
+            this.btnYeniEkle.Name = "btnYeniEkle";
+            this.btnYeniEkle.Size = new System.Drawing.Size(193, 30);
+            this.btnYeniEkle.TabIndex = 2;
+            this.btnYeniEkle.Text = "Yeni Ekle";
+            this.btnYeniEkle.UseVisualStyleBackColor = true;
+            this.btnYeniEkle.Click += new System.EventHandler(this.btnYeniEkle_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(388, 62);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(193, 30);
+            this.btnGuncelle.TabIndex = 3;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(388, 114);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(193, 30);
+            this.btnSil.TabIndex = 4;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(36, 22);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Drop Name";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(36, 53);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(56, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Drop Date";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(36, 83);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(57, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Drop Price";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(36, 118);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(77, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Party Members";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(615, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(142, 135);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // txtBoxDropName
+            // 
+            this.txtBoxDropName.Location = new System.Drawing.Point(118, 18);
+            this.txtBoxDropName.Name = "txtBoxDropName";
+            this.txtBoxDropName.Size = new System.Drawing.Size(255, 20);
+            this.txtBoxDropName.TabIndex = 10;
+            // 
+            // txtBoxPrice
+            // 
+            this.txtBoxPrice.Location = new System.Drawing.Point(118, 79);
+            this.txtBoxPrice.Name = "txtBoxPrice";
+            this.txtBoxPrice.Size = new System.Drawing.Size(255, 20);
+            this.txtBoxPrice.TabIndex = 12;
+            // 
+            // txtBoxPartyMembers
+            // 
+            this.txtBoxPartyMembers.Location = new System.Drawing.Point(118, 105);
+            this.txtBoxPartyMembers.Multiline = true;
+            this.txtBoxPartyMembers.Name = "txtBoxPartyMembers";
+            this.txtBoxPartyMembers.Size = new System.Drawing.Size(255, 38);
+            this.txtBoxPartyMembers.TabIndex = 13;
+            // 
+            // maskedTxtDropDate
+            // 
+            this.maskedTxtDropDate.Location = new System.Drawing.Point(118, 49);
+            this.maskedTxtDropDate.Name = "maskedTxtDropDate";
+            this.maskedTxtDropDate.Size = new System.Drawing.Size(255, 20);
+            this.maskedTxtDropDate.TabIndex = 14;
+            this.maskedTxtDropDate.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTxtDropDate_MaskInputRejected);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(772, 611);
+            this.Controls.Add(this.maskedTxtDropDate);
+            this.Controls.Add(this.txtBoxPartyMembers);
+            this.Controls.Add(this.txtBoxPrice);
+            this.Controls.Add(this.txtBoxDropName);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnYeniEkle);
+            this.Controls.Add(this.dataGridView2);
+            this.Name = "Form1";
+            this.Text = "TYRANNOSAURUS DROP LIST";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        
+        
+        private System.Windows.Forms.Button btnYeni;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox txtBoxFiyat;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MaskedTextBox maskedTxtTarih;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBoxDrop;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.RadioButton radioButtonEvet;
+        private System.Windows.Forms.RadioButton radioButtonHayır;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnTabloyuGetir;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button btnYeniEkle;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox txtBoxDropName;
+        private System.Windows.Forms.TextBox txtBoxPrice;
+        private System.Windows.Forms.TextBox txtBoxPartyMembers;
+        private System.Windows.Forms.MaskedTextBox maskedTxtDropDate;
+    }
+}
+
---
+++ KnigtOnlineDropTakip2021/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace KnigtOnlineDropTakip2021
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection con;
+        SqlCommand cmd;
+        string connectionString = "Server=DESKTOP-J2L0HD8\\SQLEXPRESS01;Database=KoDropDB;Trusted_Connection=True;";
+        DataSet ds;
+        SqlDataAdapter da;
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            con = new SqlConnection(connectionString);
+            maskedTxtDropDate.Text = DateTime.Now.ToString("MM/dd/yyyy");
+            txtBoxPrice.Text = ("0");
+            GetData();
+        }
+        private void GetData()
+        {
+            con.Open();
+            string Select = "Select * From DropBilgi";
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(Select, con);
+            DataTable dt = new DataTable();
+            dataAdapter.Fill(dt);
+            dataGridView2.DataSource = dt;
+            con.Close();
+        }
+        void ItemEkle()
+        {
+            try
+            {
+
+
+                var price = txtBoxPrice.Text;
+                var date = maskedTxtDropDate.Text;
+                var item = txtBoxDropName.Text;
+                var members = txtBoxPartyMembers.Text;
+
+                cmd = new SqlCommand($"insert into DropBilgi (DropName, DüştüğüTarih, Fiyat, Partydekiler) values ('{item}', '{date}', {price},'{members}')", con);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+
+                MessageBox.Show("Ohh Yeni Droplar.");
+
+                GetData();
+            }
+            catch (SqlException Ex)
+            {
+                MessageBox.Show("Hata!");
+            }
+            finally
+            {
+
+
+            }
+        }
+
+
+        private void btnYeniEkle_Click(object sender, EventArgs e)
+        {
+            ItemEkle();
+        }
+
+        private void dataGridView2_UserDeletingRow(object sender, DataGridViewRowCancelEventArgs e)
+        {
+            var result = MessageBox.Show("Silmek istediğinize emin misiniz?", "Dikkat!", MessageBoxButtons.YesNo);
+            if (result == DialogResult.No)
+            {
+                return;
+            }
+
+            var id = e.Row.Cells["id"].Value;
+            cmd = new SqlCommand($"Delete from DropBilgi Where id = {id}", con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Başarıyla silindi!");
+        }
+
+        private void dataGridView2_ColumnAdded(object sender, DataGridViewColumnEventArgs e)
+        {
+            if (e.Column.Name == "id")
+            {
+                e.Column.HeaderText = "Sıra";
+            }
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in dataGridView2.SelectedRows)
+            {
+                MessageBox.Show("Silmek istediğinize emin misiniz?", "Dikkat!", MessageBoxButtons.YesNo);
+                var id = row.Cells["id"].Value;
+                string sql = $"Delete from DropBilgi Where id = {id}";
+                cmd = new SqlCommand(sql, con);
+                cmd.Parameters.AddWithValue($"@id", id);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+                GetData();
+
+            }
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://www.nttgameonline.com/knight/tr/ranking/clan/5/Elmo");
+        }
+
+        private void maskedTxtDropDate_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+    }
+}
+                                   //DropName, DüştüğüTarih, Fiyat, Partydekiler
